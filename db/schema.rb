8c2099be59079aa4f86e1a84ee6c7d38984a839f
long.levi823 <long.levi823@gmail.com>
2019-05-31 21:03:55 -0400

    Create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_184954) do
+ActiveRecord::Schema.define(version: 2019_06_01_005741) do
 
   create_table "medications", force: :cascade do |t|
     t.string "medicationname"
     t.string "unitvalue"
     t.string "unitmeasurement"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
