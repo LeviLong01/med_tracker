bde211e3555febdb7b77f08221acef95621ecfc3
long.levi823 <long.levi823@gmail.com>
2019-06-01 12:34:42 -0400

    Added secure password to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_161111) do
+ActiveRecord::Schema.define(version: 2019_06_01_163039) do
 
   create_table "medications", force: :cascade do |t|
     t.string "medicationname"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_06_01_161111) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
