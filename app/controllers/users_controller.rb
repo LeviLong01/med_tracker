d43bc5e7fe587e119b0a1c11367e1af84c28a3bd
long.levi823 <long.levi823@gmail.com>
2019-06-02 13:46:18 -0400

    Added pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @user_medications = @user.medications.paginate(page: params[:page], per_page: 5)
   end
 
   private
