baa5ed03de437f2ed1950528b263cb64d2b70c0c
long.levi823 <long.levi823@gmail.com>
2019-06-05 00:03:31 -0400

    Updated sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Thank you for signing up with MedTracker #{@user.username}"
-      redirect_to user_path
+      redirect_to root_path
     else
       render 'new'
     end
